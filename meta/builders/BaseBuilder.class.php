92f23a2322cea6fd46bf1b278c93f169336cdbfc
dewid <dewid_1985@mail.ru>
2016-11-21 16:11:24 +0300

    add namespace

--- meta/builders/BaseBuilder.class.php
+++ meta/builders/BaseBuilder.class.php
@@ -57,7 +57,7 @@ public function getObjectName()
 
 public function getSequence()
 {
-    return '{$class->getSchemaAndTableName()}_seq';
+    return '{$class->getSchemaAndTableName()}_id_seq';
 }
 EOT;
             } elseif ($class->getWithInternalProperties()) {
