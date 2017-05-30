bbaec169254d6587e7443a6a890486fe5a7e8a51
dewid <dewid_1985@mail.ru>
2017-05-30 10:36:54 +0300

    refactor JsonXssView

--- main/UI/View/JsonXssView.class.php
+++ main/UI/View/JsonXssView.class.php
@@ -12,7 +12,7 @@ namespace OnPhp {
     /**
      * @ingroup Flow
      **/
-    class JsonXssView extends JsonPView
+    class JsonXssView implements View, Stringable
     {
         /**
          * Javascript valid function name pattern
@@ -29,7 +29,23 @@ namespace OnPhp {
          * Default callback
          * @var string
          */
-        protected $callback = 'name';
+        protected $variable = 'data';
+
+        /**
+         * @var int
+         */
+        protected $options = 0;
+
+        /**
+         * @param string $variable
+         * @return $this
+         */
+        public function setVariable(string $variable)
+        {
+            $this->variable = $variable;
+            return $this;
+        }
+
 
         /**
          * @param $value
@@ -43,6 +59,106 @@ namespace OnPhp {
             }
 
             $this->prefix = $value;
+
+            return $this;
+        }
+
+
+        public function setHexQuot($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_HEX_QUOT;
+            } else {
+                $this->options = $this->options & ~JSON_HEX_QUOT;
+            }
+
+            return $this;
+        }
+
+
+        public function setHexTag($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_HEX_TAG;
+            } else {
+                $this->options = $this->options & ~JSON_HEX_TAG;
+            }
+
+            return $this;
+        }
+
+
+        public function setHexAmp($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_HEX_AMP;
+            } else {
+                $this->options = $this->options & ~JSON_HEX_AMP;
+            }
+
+            return $this;
+        }
+
+
+        public function setHexApos($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_HEX_APOS;
+            } else {
+                $this->options = $this->options & ~JSON_HEX_APOS;
+            }
+
+            return $this;
+        }
+
+
+        public function setForceObject($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_FORCE_OBJECT;
+            } else {
+                $this->options = $this->options & ~JSON_FORCE_OBJECT;
+            }
+
+            return $this;
+        }
+
+
+        public function setNumericCheck($flag = false)
+        {
+            if ($flag) {
+                $this->options = $this->options | JSON_NUMERIC_CHECK;
+            } else {
+                $this->options = $this->options & ~JSON_NUMERIC_CHECK;
+            }
+
+            return $this;
+        }
+
+
+        public function setPrettyPrint($flag = false)
+        {
+            if (defined("JSON_PRETTY_PRINT")) {
+                if ($flag) {
+                    $this->options = $this->options | JSON_PRETTY_PRINT;
+                } else {
+                    $this->options = $this->options & ~JSON_PRETTY_PRINT;
+                }
+            }
+
+            return $this;
+        }
+
+
+        public function setUnescapedSlashes($flag = false)
+        {
+            if (defined("JSON_UNESCAPED_SLASHES")) {
+                if ($flag) {
+                    $this->options = $this->options | JSON_UNESCAPED_SLASHES;
+                } else {
+                    $this->options = $this->options & ~JSON_UNESCAPED_SLASHES;
+                }
+            }
 
             return $this;
         }
@@ -61,7 +177,7 @@ namespace OnPhp {
             $this->setHexQuot(true);
             $this->setHexTag(true);
 
-            $json = JsonView::toString($model);
+            $json = $this->wrap($model);
 
             $json = str_ireplace(
                 ['u0022', 'u0027'],
@@ -69,12 +185,37 @@ namespace OnPhp {
                 $json
             );
 
-            $result = '<script type="text/javascript">' . "\n";
-            $result .= "\t" . $this->prefix . $this->callback . '=\'' . $json . '\';' . "\n";
-            $result .= '</script>' . "\n";
-
+            $result = "\t" . $this->prefix . $this->variable . '=\'' . $json . '\';' . "\n";
             return $result;
         }
 
+        /**
+         * @param Model $model
+         * @return string
+         */
+        public function wrap($model = null) : string
+        {
+            Assert::isTrue($model === null || $model instanceof Model);
+            if ($this->options) {
+                return json_encode($model ? $model->getList() : [], $this->options);
+            } else {
+                return json_encode($model ? $model->getList() : []);
+            }
+        }
+
+        /**
+         * @param null $model
+         * @return $this
+         */
+        public function render($model = null)
+        {
+            if (!headers_sent()) {
+                header('Content-Type: ' . 'text/javascript');
+            }
+
+            echo $this->toString($model);
+
+            return $this;
+        }
     }
 }
